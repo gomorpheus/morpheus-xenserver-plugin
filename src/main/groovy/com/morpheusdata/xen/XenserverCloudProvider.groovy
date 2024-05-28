ec13bc10a50d0e151461660003a33bffb9ca9888
razi-ahmad1 <160714505+razi-ahmad1@users.noreply.github.com>
2024-05-28 18:18:49 +0530

    Network Type missing (#45)
    * missing network types

--- src/main/groovy/com/morpheusdata/xen/XenserverCloudProvider.groovy
+++ src/main/groovy/com/morpheusdata/xen/XenserverCloudProvider.groovy
@@ -180,6 +180,24 @@ class XenserverCloudProvider implements CloudProvider {
 		Collection<NetworkType> networks = context.services.network.list(new DataQuery().withFilter(
 				'code','in', ['dockerBridge', 'host', 'overlay']))
 
+		networks << new NetworkType([
+				code				: 'xenNetwork',
+				name				: 'Xen Network',
+				description			: '',
+				overlay				: false,
+				externalType		: 'External',
+				creatable			: false,
+				cidrEditable		: true,
+				dhcpServerEditable	: true,
+				dnsEditable			: true,
+				gatewayEditable		: true,
+				cidrRequired		: false,
+				vlanIdEditable		: true,
+				canAssignPool		: true,
+				hasNetworkServer	: false,
+				hasCidr				: true
+		])
+
 		return networks
 	}
 
