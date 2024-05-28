b7886e1dce9502a706a6f146d577deb600bcdccf
razi-ahmad1 <razi.ahmad@telusinternational.com>
2024-05-28 18:15:36 +0530

    xenNetwork added

--- src/main/groovy/com/morpheusdata/xen/XenserverCloudProvider.groovy
+++ src/main/groovy/com/morpheusdata/xen/XenserverCloudProvider.groovy
@@ -178,7 +178,25 @@ class XenserverCloudProvider implements CloudProvider {
 	Collection<NetworkType> getNetworkTypes() {
 
 		Collection<NetworkType> networks = context.services.network.list(new DataQuery().withFilter(
-				'code','in', ['dockerBridge', 'host', 'overlay', 'xenNetwork']))
+				'code','in', ['dockerBridge', 'host', 'overlay']))
+
+		networks << new NetworkType([
+				code				: 'xenNetwork',
+				name				: 'Xen Network',
+				description			: '',
+				overlay				: false,
+				externalType		: 'External',
+				creatable			: false,
+				cidrEditable		: true,
+				dhcpServerEditable	: true,
+				dnsEditable			: true,
+				gatewayEditable		: true,
+				cidrRequired		: false,
+				vlanIdEditable		: true,
+				canAssignPool		: true,
+				hasNetworkServer	: false,
+				hasCidr				: true
+		])
 
 		return networks
 	}
