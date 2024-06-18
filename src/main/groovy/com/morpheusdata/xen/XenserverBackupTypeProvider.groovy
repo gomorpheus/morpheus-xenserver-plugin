642656d58cde0719607e1657dc54ea99dd809249
Dustin Deyoung <ddeyoung@morpheusdata.com>
2024-06-17 21:06:29 -0400

    Add snapshot flag to backup type provider.

--- src/main/groovy/com/morpheusdata/xen/XenserverBackupTypeProvider.groovy
+++ src/main/groovy/com/morpheusdata/xen/XenserverBackupTypeProvider.groovy
@@ -103,6 +103,16 @@ class XenserverBackupTypeProvider extends AbstractBackupTypeProvider {
 	}
 
 	/**
+	 * Determines if this backup type relies on a snapshot process. Snapshots are typically used for quick backups and
+	 * are not designed for long-term storage. For long-term backup creation, additional processes, such as copy to store,
+	 * should be utilized by providers that rely on snapshots.
+	 */
+	@Override
+	Boolean isSnapshot() {
+		return true
+	}
+
+	/**
 	 * Get the desired method of restoring a backup to a new instance.
 	 * <p>
 	 * Available options:
