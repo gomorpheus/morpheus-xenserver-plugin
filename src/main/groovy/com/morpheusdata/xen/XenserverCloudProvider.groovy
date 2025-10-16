ab0dbe55b1ab75aa3db5244a1ee00e18a373113e
Sydney Lim <sydney@morpheusdata.com>
2025-10-16 11:51:27 -0700

    [PCCP-6750] sl - enable security group active checkbox on cloud

--- src/main/groovy/com/morpheusdata/xen/XenserverCloudProvider.groovy
+++ src/main/groovy/com/morpheusdata/xen/XenserverCloudProvider.groovy
@@ -527,6 +527,10 @@ class XenserverCloudProvider implements CloudProvider {
 		return true
 	}
 
+	static Boolean hasSecurityGroups() {
+		return true
+	}
+
 	/**
 	 * Called when a server should be started. Returning a response of success will cause corresponding updates to usage
 	 * records, result in the powerState of the computeServer to be set to 'on', and related instances set to 'running'
